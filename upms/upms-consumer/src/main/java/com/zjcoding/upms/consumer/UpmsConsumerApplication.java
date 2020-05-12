cff8cf5b1a1646728e4bfaef3af6b4274b1057ff
luckybearbear <jun.zhang@pexetech.com>
2020-05-12 15:07:20 +0800

    :tada: 集成 logback 日志

--- upms/upms-consumer/src/main/java/com/zjcoding/upms/consumer/UpmsConsumerApplication.java
+++ upms/upms-consumer/src/main/java/com/zjcoding/upms/consumer/UpmsConsumerApplication.java
@@ -4,7 +4,11 @@ import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * 启动器
@@ -20,5 +24,14 @@ public class UpmsConsumerApplication {
         ConfigurableApplicationContext context = SpringApplication.run(UpmsConsumerApplication.class, args);
         int length = context.getBeanDefinitionNames().length;
         log.info("Spring boot启动初始化了 {} 个 Bean", length);
+        try {
+            String host = InetAddress.getLocalHost().getHostAddress();
+            TomcatServletWebServerFactory tomcatServletWebServerFactory= (TomcatServletWebServerFactory) context.getBean("tomcatServletWebServerFactory");
+            int port = tomcatServletWebServerFactory.getPort();
+            String contextPath = tomcatServletWebServerFactory.getContextPath();
+            log.info("swagger-ui.html访问地址={}","http://"+host+":"+port+contextPath+"/swagger-ui.html");
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
     }
 }
